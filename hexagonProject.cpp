53be257faf7f5ec67b979be9d61cba9ffa5d32c5
ferai <forgotten_dreams@abv.bg>
2016-06-25 17:34:59 +0300

    Initial Commit

---
+++ hexagonProject.cpp
@@ -0,0 +1,15 @@
+// hexagonProject.cpp : Defines the entry point for the console application.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int aa;
+	cout << "AA\n";
+	cin >> aa;
+
+
+	return 0;
+}
+
